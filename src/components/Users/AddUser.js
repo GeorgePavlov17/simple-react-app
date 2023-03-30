72fb2fe0ca68e58f72eea75aa0fe573752addd32
Georgi Pavlov <georgipavlov17@gmail.com>
2023-03-30 22:43:02 +0300

    fix

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import Card from "./UI/Card";
+import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
-import Button from "./UI/Button";
+import Button from "../UI/Button";
 
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
